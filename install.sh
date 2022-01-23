9b38d1b7d858becca654ba89d75f2671ca44aa38
S9yd3R <osgod443@gmail.com>
2022-01-23 15:35:41 +0530

    1/2

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 #colors
 red="\033[1;91m"
 cyan="\033[1;36m"
-grey="\033[1;90m"
 green="\033[1;92m"
-blink="\e[5m"
-magenta="\033[1;95m"
+reset="\033[0m"
+
+
+#functions
 
 change_theme() {
 	cat > ~/.termux/colors.properties <<- _EOF_
@@ -32,22 +33,61 @@ change_theme() {
 		color14=#56b6c2
 		color15=#c8ccd4
 	_EOF_
+	rm -rf ~/.termux/font.ttf
+	cp ./.files/font.ttf ~/.termux
+}
 
+properties() {
 	cat > ~/.termux/termux.properties <<- _EOF_
+	extra-keys = [ \
+    ['F1' , 'ESC', 'CTRL', 'ALT', 'TAB', {key: KEYBOARD, popup: DRAWER}, 'HOME', 'UP', 'END'], \
+    ['DELETE', '{}', '()', '[]', '$', 'BACKSLASH', 'LEFT', 'DOWN', 'RIGHT'] \
+]
 
+# fullscreen = true
+
+# use-fullscreen-workaround = true
+
+# Open a new terminal with ctrl + t (volume down + t)
+shortcut.create-session = ctrl + t
+
+# Go one session down with (for example) ctrl + 2
+shortcut.next-session = ctrl + 2
+
+# Go one session up with (for example) ctrl + 1
+shortcut.previous-session = ctrl + 1
+
+# Rename a session with (for example) ctrl + n
+shortcut.rename-session = ctrl + u
+
+# Beep with a sound.
+# bell-character=beep
+
+# Cursor Blinking
+terminal-cursor-blink-rate=600
 	_EOF_
 }
 
-
-
 install_pkgs() {
-	pkgs=(x11-repo xfce4 polybar tigervnc geany audacious)
-	for i in "${pkgs[@]}" ; do
-		if ! hash ${i} > /dev/null 2>&1;
+	pkgs=(lsd x11-repo xfce4 polybar tigervnc geany audacious)
+	for pkg_ in "${pkgs[@]}" ; do
+		if ! hash ${pkg_} > /dev/null 2>&1;
 		then
-			echo -e ""
-			apt install "${i}" -y
+			sp=( "\033[1;91m⬤   " "\033[1;91m⬤  \033[1;92m⬤" "   \033[1;92m⬤" "\033[0m    ")
+
+        		for i in "${sp[@]}" ; do
+        			echo -ne "\rInstalling $pkg_   ${i}"
+					sleep 1
+				done
 		fi
 	done
 }
 
+echo -e "${cyan}Installing required pkgs this may take a while . Grab a cup of coffee${reset} \n"
+
+change_theme
+properties
+echo "alias ls=\"lsd\"" >> $PREFIX/etc/bash.bashrc
+$SHELL
+termux-reload-settings
+install_pkgs
